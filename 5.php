655c94693033e1ebe547bc7ed9b4b235f37cbc0c
vrg6875 <vickyrajgupta2003@gmail.com>
2024-10-27 18:18:59 +0530

    first

---
+++ 5.php
@@ -0,0 +1,11 @@
+<?php 
+$name='raj gupta';
+$income=700;
+echo "name is $name <br> income is $income"."<br>";
+echo "name is"  .  "vicky"."<br>";
+echo "word count is " , str_word_count($name)."<br>";
+echo strrev($name);
+
+$income +=10;
+echo $income;
+?> 
---
+++ if-else.php
@@ -0,0 +1,11 @@
+<?php
+$a = 10;
+$b = 1;
+
+if ($a >$b) {
+    echo "$a is greater";
+} 
+ else {
+    echo "b is.";
+}
+?>
